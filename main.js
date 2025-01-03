7bfc33f66515682284903cb34c24e17e84ad2e6a
GauravPandey0065 <pg633247@gmail.com>
2025-01-04 00:49:56 +0530

    netlify

--- main.js
+++ main.js
@@ -41,6 +41,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server on port 3000
-server.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
     console.log('Server running on http://localhost:3000');
 });
